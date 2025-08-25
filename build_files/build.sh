152538ee9cbe699d0c369e85c69f1fb599c45bfe
grondalf <vuk.hidalgo@gmail.com>
2025-08-25 15:00:05 +0200

    Autoconfirm transactions.

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,11 +20,13 @@ set -ouex pipefail
 # dnf5 -y copr disable ublue-os/staging
 
 # Install RPM Fusion Repos:
-dnf5 install \   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf5 install \   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install \   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf5 -y install \   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+dnf5 -y upgrade --refresh 
 
 # Install the propietary NVIDIA drivers:
-dnf5 install akmod-nvidia xorg-x11-drv-nvidia-cuda
+dnf5 -y install akmod-nvidia xorg-x11-drv-nvidia-cuda
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
