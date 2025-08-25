e8661bf255d9200a601767755a5dc1dc27fd3e69
grondalf <vuk.hidalgo@gmail.com>
2025-08-25 16:57:43 +0200

    Added some more packages to be installed.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,36 +2,38 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-# dnf5 install -y tmux
-
-# Use a COPR Example:
-#
 # dnf5 -y copr enable ublue-os/staging
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-# Install the propietary NVIDIA drivers:
+# Enable RPM Fusion repos:
 dnf5 install -y \
 	https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
 	https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-  
+	
+# Enable Negativo17 repo for Samsung ULD Drivers:
+dnf5 config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-uld.repo --save-filename=negativo17-uld.repo
+
+# Import Insync GPG key and add its repository:
+rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key && echo -e "[insync]\nname=insync repo\nbaseurl=http://yum.insync.io/fedora/\$releasever/\ngpgcheck=1\ngpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key\nenabled=1\nmetadata_expire=120m" | sudo tee /etc/yum.repos.d/insync.repo > /dev/null
+
+# Import COPR repos:
+dnf5 copr enable -y \
+	lukenukem/asus-linux 
+
+# Install packages:  
 dnf5 install -y \
 	akmod-nvidia \
 	xorg-x11-drv-nvidia-cuda \
+	uld \
+	insync \
 	intel-undervolt \
 	steam-devices \
 	papirus-icon-theme
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
+systemctl enable intel-undervolt
 
 
